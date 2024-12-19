85141bbe648d343f985937cd33b850d483ef27a9
Oruj Gafarov <65281943+orrstudio@users.noreply.github.com>
2024-12-19 05:48:04 +0400

    Обновлен README и main.py, добавлен HLS-плеер

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # Hidayet Televizionları
 
-[MPLTV](ttp://ibrahimiptv.com:1935/mpltv/mpltv/playlist.m3u8)
-[NURTV](ttp://ibrahimiptv.com:1935/nurtv/nurtv/playlist.m3u8)
-[HERAN KURAN HERAN MUTLULUK](ttp://ibrahimiptv.com:1935/herankuran/herankuran/playlist.m3u8)
-[HERAN KURAN HERAN ZİKİR](ttp://ibrahimiptv.com:1935/heranzikir/heranzikir/playlist.m3u8)
-[KURAN LAFZI VE 7 RUHU](ttp://ibrahimiptv.com:1935/kuran/kuran/playlist.m3u8)
+[MPLTV](http://ibrahimiptv.com:1935/mpltv/mpltv/playlist.m3u8)
+[NURTV](http://ibrahimiptv.com:1935/nurtv/nurtv/playlist.m3u8)
+[HERAN KURAN HERAN MUTLULUK](http://ibrahimiptv.com:1935/herankuran/herankuran/playlist.m3u8)
+[HERAN KURAN HERAN ZİKİR](http://ibrahimiptv.com:1935/heranzikir/heranzikir/playlist.m3u8)
+[KURAN LAFZI VE 7 RUHU](http://ibrahimiptv.com:1935/kuran/kuran/playlist.m3u8)
 
 
 # Hidayet Radioları
 
-[ABRAHAM TV ALMANCA](ttp://ibrahimiptv.com:1935/abraham/abraham/playlist.m3u8)
-[ABRAHAM TV İNGİLİZCE](ttp://ibrahimiptv.com:1935/hak_en/hak_en/playlist.m3u8)
-[ABRAHAM TV RUSÇA](ttp://ibrahimiptv.com:1935/hak_ru/hak_ru/playlist.m3u8)
-[ABRAHAM TV ARAPÇA](ttp://ibrahimiptv.com:1935/hak_ar/hak_ar/playlist.m3u8)
-[ABRAHAM TV KÜRTÇE](ttp://ibrahimiptv.com:1935/hak_kr/hak_kr/playlist.m3u8)
-[ABRAHAM TV FRANSIZCA](ttp://ibrahimiptv.com:1935/hak_fr/hak_fr/playlist.m3u8)
-[ABRAHAM TV İSPANYOLCA](ttp://ibrahimiptv.com:1935/hak_es/hak_es/playlist.m3u8)
-[ABRAHAM TV ÇİNCE](ttp://ibrahimiptv.com:1935/hak_ch/hak_ch/playlist.m3u8)
-[ABRAHAM TV BULGARCA](ttp://ibrahimiptv.com:1935/hak_bg/hak_bg/playlist.m3u8)
-[ABRAHAM TV FLEMENKÇE](ttp://ibrahimiptv.com:1935/hak_ne/hak_ne/playlist.m3u8)
-[ABRAHAM TV Farsça](ttp://ibrahimiptv.com:1935/hak_fa/hak_fa/playlist.m3u8)
+[ABRAHAM TV ALMANCA](http://ibrahimiptv.com:1935/abraham/abraham/playlist.m3u8)
+[ABRAHAM TV İNGİLİZCE](http://ibrahimiptv.com:1935/hak_en/hak_en/playlist.m3u8)
+[ABRAHAM TV RUSÇA](http://ibrahimiptv.com:1935/hak_ru/hak_ru/playlist.m3u8)
+[ABRAHAM TV ARAPÇA](http://ibrahimiptv.com:1935/hak_ar/hak_ar/playlist.m3u8)
+[ABRAHAM TV KÜRTÇE](http://ibrahimiptv.com:1935/hak_kr/hak_kr/playlist.m3u8)
+[ABRAHAM TV FRANSIZCA](http://ibrahimiptv.com:1935/hak_fr/hak_fr/playlist.m3u8)
+[ABRAHAM TV İSPANYOLCA](http://ibrahimiptv.com:1935/hak_es/hak_es/playlist.m3u8)
+[ABRAHAM TV ÇİNCE](http://ibrahimiptv.com:1935/hak_ch/hak_ch/playlist.m3u8)
+[ABRAHAM TV BULGARCA](http://ibrahimiptv.com:1935/hak_bg/hak_bg/playlist.m3u8)
+[ABRAHAM TV FLEMENKÇE](http://ibrahimiptv.com:1935/hak_ne/hak_ne/playlist.m3u8)
+[ABRAHAM TV Farsça](http://ibrahimiptv.com:1935/hak_fa/hak_fa/playlist.m3u8)
+
+
+"Radyo Nur": "https://canli.hidayetradyolari.com/listen/radyo_nur/radio.mp3",
+"MPL Radyo": "https://canli.hidayetradyolari.com/listen/mpl_radyo/radio.mp3",
--- main.py
+++ main.py
@@ -1,82 +1,35 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.image import Image
-from jnius import autoclass
-from time import sleep
+from kivy.uix.textinput import TextInput
+from kivy.uix.video import Video
 
-# Список радиостанций
-RADIO_STATIONS = {
-    "Radyo Nur": "https://canli.hidayetradyolari.com/listen/radyo_nur/radio.mp3",
-    "MPL Radyo": "https://canli.hidayetradyolari.com/listen/mpl_radyo/radio.mp3",
-}
-
-# Получаем доступ к классам Android
-MediaPlayer = autoclass('android.media.MediaPlayer')
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-
-
-class RadioPlayer(FloatLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        # Фоновое изображение
-        self.add_widget(Image(source='images/background.jpg', fit_mode='fill'))
-
-        # Верхний текст состояния
-        self.status_label = Label(text="Select a radio station", font_size=20, color=(1, 1, 1, 1),
-                                  size_hint=(1, 0.1), pos_hint={"x": 0, "y": 0.85})
-        self.add_widget(self.status_label)
-
-        # Контейнер для кнопок
-        button_layout = FloatLayout(size_hint=(1, 0.2), pos_hint={"center_x": 0.5, "y": 0.05})
-        self.add_widget(button_layout)
-
-        # Создаем кнопки для каждой радиостанции
-        button_y_pos = 0.7  # Начальная позиция для кнопок
-        for name, url in RADIO_STATIONS.items():
-            button = Button(text=name, font_size=18, size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5, "y": button_y_pos})
-            button.bind(on_press=self.create_on_press_handler(name, url))
-            button_layout.add_widget(button)
-
-            button_y_pos -= 0.25  # Сдвигаем кнопку ниже
-
-        # Инициализация проигрывателя
-        self.player = None
-
-    def create_on_press_handler(self, name, url):
-        """Создаем обработчик нажатия с правильными параметрами."""
-        def on_press(instance):
-            self.play_radio(name, url)
-        return on_press
-
-    def play_radio(self, name, url):
-        # Останавливаем текущий проигрыватель, если есть
-        if self.player:
-            self.player.stop()
-
-        # Обновляем статус
-        self.status_label.text = f"Playing: {name}"
-
-        # Создаем новый плеер для выбранного потока
-        self.player = MediaPlayer()
-        self.player.setDataSource(url)
-        self.player.prepare()  # Подготовка плеера
-        self.player.start()    # Запуск воспроизведения
-
-    def stop_radio(self):
-        if self.player:
-            self.player.stop()
-            self.status_label.text = "Select a radio station"
-
-
-class RadioApp(App):
+class HLSPlayerApp(App):
     def build(self):
-        self.title = "Radio Player"
-        return RadioPlayer()
+        self.stream_url = 'http://ibrahimiptv.com:1935/abraham/abraham/playlist.m3u8'
+        
+        self.layout = BoxLayout(orientation='vertical')
+        
+        self.url_input = TextInput(hint_text='Введите URL HLS потока')
+        self.layout.add_widget(self.url_input)
+        
+        self.play_button = Button(text='Воспроизвести')
+        self.play_button.bind(on_press=self.play_stream)
+        self.layout.add_widget(self.play_button)
+        
+        self.video = Video()
+        self.layout.add_widget(self.video)
+        
+        return self.layout
 
+    def play_stream(self, instance):
+        self.stream_url = self.url_input.text
+        if self.stream_url:
+            self.video.source = self.stream_url
+            self.video.state = 'play'
+        else:
+            self.layout.add_widget(Label(text='Введите правильный URL!'))
 
-if __name__ == "__main__":
-    RadioApp().run()
-
+if __name__ == '__main__':
+    HLSPlayerApp().run()
